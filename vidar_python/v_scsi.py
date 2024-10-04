80b769a84e62895e9b59d5cb3373570892e943d4
Toni Magni <email@antoniomagni.com>
2024-10-04 03:37:53 -0400

    Prepare for more convos.

--- vidar_python/v_scsi.py
+++ vidar_python/v_scsi.py
@@ -2,26 +2,30 @@
 
 Keep lean from USB stuff.
 """
-
 from vidar_python.v_usb import build_cbw
 
-# SCSI Inquiry Command (from the dump)
-SCSI_INQUIRY_CDB = [
-    0x12,  # Opcode for SCSI Inquiry
-    0x00,  # CMDT, EVPD flags
-    0x00,  # Reserved
-    0x00,  # Reserved
-    52,    # Allocation length (from the dump)
-    0x00   # Control byte (default)
-]
+SCSI_TEST_UNIT_READY = 0x0
+SCSI_INQUIRY = 0x12
+SCSI_RECEIVE_DIAGNOSTIC_RESULTS = 0x1cb
+SCSI_SEND_DIAGNOSTIC = 0x1d
 
-# Convert SCSI command to bytes
-scsi_command_bytes = bytes(SCSI_INQUIRY_CDB) + \
-    b'\x00' * (16 - len(SCSI_INQUIRY_CDB))
+def build_scsi_command_bytes(command):
+    cdb = [
+        command,  # Opcode for SCSI Inquiry
+        0x00,  # CMDT, EVPD flags
+        0x00,  # Reserved
+        0x00,  # Reserved
+        52,    # Allocation length (from the dump)
+        0x00   # Control byte (default)
+    ]
+    return bytes(cdb) + b'\x00' * (16 - len(cdb))
 
+
+def build_cbd(command):
+    return build_cbw() + build_scsi_command_bytes(command)
 
 def build_inquiry_cbd():
-    return build_cbw() + scsi_command_bytes
+    return build_cbd(SCSI_INQUIRY)
 
 def parse_scsi_inquiry_response(response):
     # Create a dictionary to store the inquiry data
